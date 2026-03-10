87b8da5ccb715340dbdd0be94e0337068a1d24fd
siddharthasiddu11 <siddullll6@gmail.com>
2026-03-10 22:23:32 +0530

    added packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,52 @@
 import express from "express";
+import  jwt  from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import {createRoomSchema, CreateUserSchema, SignInSchema} from "@repo/common/types"
 
 const app = express();
 
+app.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+})
+app.post("/signin", (req, res) => {
+
+    const data = SignInSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+   const userId = 1;
+   const token = jwt.sign({
+    userId
+   }, JWT_SECRET)
+
+   res.json({
+    token
+   })
+
+})
+app.post("/room",middleware, (req, res) => {
+    const data = createRoomSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    res.json({
+        roomId: 123,
+    })
+ 
+})
+
 app.listen(3000);
+
